137d1e1ab30a36f6b75c705cf3927e69188bd038
Kristina Spasevska <kristina.spasevska@deptagency.com>
2024-08-06 23:28:05 +0200

    feat: add slogan as nested content

--- src/backend/DTNL.UmbracoCms.Web/Components/Slogan/Slogan.cs
+++ src/backend/DTNL.UmbracoCms.Web/Components/Slogan/Slogan.cs
@@ -1,3 +1,5 @@
+using Umbraco.Cms.Web.Common.PublishedModels;
+
 namespace DTNL.UmbracoCms.Web.Components;
 
 public class Slogan
@@ -7,4 +9,28 @@ public class Slogan
     public bool AnimateOnScroll { get; set; }
 
     public string? CssClasses { get; set; }
+
+    public static Slogan? Create(Umbraco.Cms.Web.Common.PublishedModels.Slogan slogan, string? cssClasses = null)
+    {
+        if (slogan.Text.IsNullOrWhiteSpace())
+        {
+            return null;
+        }
+
+        return new Slogan { Text = slogan.Text ?? "", AnimateOnScroll = slogan.AnimateOnScroll, CssClasses = cssClasses };
+    }
+
+    public static Slogan? Create(NestedBlockSlogan block, string? cssClasses = null)
+    {
+        Umbraco.Cms.Web.Common.PublishedModels.Slogan? slogan = block?.Slogan?.Select(block => block.Content).OfType<Umbraco.Cms.Web.Common.PublishedModels.Slogan>().FirstOrDefault();
+
+        if (slogan is not { } scrollingText)
+        {
+            return null;
+        }
+
+        return new Slogan { Text = scrollingText?.Text ?? "", AnimateOnScroll = scrollingText!.AnimateOnScroll, CssClasses = cssClasses };
+    }
+
+
 }
