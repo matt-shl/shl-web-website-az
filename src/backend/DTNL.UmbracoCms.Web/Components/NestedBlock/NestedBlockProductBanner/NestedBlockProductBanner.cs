a479d1f5747f6be1984efe7b6732999e95febf98
Pedro Pontes <pedro.pontes@deptagency.com>
2024-08-06 14:12:39 +0200

    bug fixing

--- src/backend/DTNL.UmbracoCms.Web/Components/NestedBlock/NestedBlockProductBanner/NestedBlockProductBanner.cs
+++ src/backend/DTNL.UmbracoCms.Web/Components/NestedBlock/NestedBlockProductBanner/NestedBlockProductBanner.cs
@@ -4,7 +4,7 @@ namespace DTNL.UmbracoCms.Web.Components.NestedBlock;
 
 public class NestedBlockProductBanner : NestedBlockWithInner
 {
-    protected override CardCarousel? GetInnerComponent(IPublishedElement block)
+    protected override ProductDescription? GetInnerComponent(IPublishedElement block)
     {
         if (block is not Umbraco.Cms.Web.Common.PublishedModels.NestedBlockProductBanner productBannerBlock)
         {
