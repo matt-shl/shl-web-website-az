5d6debf43deaf3d283599287c4c618ab6cc8d528
Kristina Spasevska <kristina.spasevska@deptagency.com>
2024-08-19 23:20:39 +0200

    chore: change blocks to nested blocks

--- src/backend/DTNL.UmbracoCms.Web/Components/NestedBlock/NestedBlockTextMediaBanner/NestedBlockTextMediaBanner.cs
+++ src/backend/DTNL.UmbracoCms.Web/Components/NestedBlock/NestedBlockTextMediaBanner/NestedBlockTextMediaBanner.cs
@@ -1,13 +1,12 @@
 using Umbraco.Cms.Core.Models.PublishedContent;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DTNL.UmbracoCms.Web.Components.NestedBlock.TextMediaBanner;
 
-public class TextMediaBanner : NestedBlockWithInner
+public class NestedBlockTextMediaBanner : NestedBlockWithInner
 {
     protected override BannerTextMedia? GetInnerComponent(IPublishedElement block)
     {
-        if (block is not Umbraco.Cms.Web.Common.PublishedModels.TextMediaBanner textMediaBannerBlock)
+        if (block is not Umbraco.Cms.Web.Common.PublishedModels.NestedBlockTextMediaBanner textMediaBannerBlock)
         {
             return null;
         }
--- src/backend/DTNL.UmbracoCms.Web/umbraco/models/NestedBlockTextMediaBanner.generated.cs
+++ src/backend/DTNL.UmbracoCms.Web/umbraco/models/NestedBlockTextMediaBanner.generated.cs
@@ -19,13 +19,13 @@ using Umbraco.Extensions;
 namespace Umbraco.Cms.Web.Common.PublishedModels
 {
 	/// <summary>Text Media Banner</summary>
-	[PublishedModel("textMediaBanner")]
-	public partial class TextMediaBanner : PublishedElementModel
+	[PublishedModel("nestedBlockTextMediaBanner")]
+	public partial class NestedBlockTextMediaBanner : PublishedElementModel
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
-		public new const string ModelTypeAlias = "textMediaBanner";
+		public new const string ModelTypeAlias = "nestedBlockTextMediaBanner";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
@@ -34,14 +34,14 @@ namespace Umbraco.Cms.Web.Common.PublishedModels
 			=> PublishedModelUtility.GetModelContentType(publishedSnapshotAccessor, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "13.2.0+7dff3a3")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<TextMediaBanner, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<NestedBlockTextMediaBanner, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(publishedSnapshotAccessor), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public TextMediaBanner(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public NestedBlockTextMediaBanner(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
