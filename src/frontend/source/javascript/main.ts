979b089862b21e57306c35869b3df4a1ff0aaa62
CU DEPT Anne Vandenhoogen <cu.dept.anne.vandenhoogen@shlgroup.onmicrosoft.com>
2024-08-21 09:29:38 +0000

    Merged PR 1499: Map

--- src/frontend/source/javascript/main.ts
+++ src/frontend/source/javascript/main.ts
@@ -18,7 +18,7 @@ import '@/components/loading-indicator'
 
 import Events from '@utilities/events'
 
-import { videoLoader } from '@/components/video'
+import {videoLoader} from '@/components/video'
 import moduleInit from '@/utilities/module-init'
 
 if (document.querySelector('[js-hook-page-load-animation-trigger]')) {
@@ -40,6 +40,7 @@ moduleInit.async('[js-hook-rich-text]', () => import('@/components/rich-text'))
 moduleInit.async('[js-hook-banner-quote]', () => import('@components/banner-quote'))
 moduleInit.async('[js-hook-carousel-indicator]', () => import('@components/carousel-indicator'))
 moduleInit.async('[js-hook-form]', () => import('@/components/form'))
+moduleInit.async('[js-hook-map]', () => import('@/components/map'))
 moduleInit.async('[js-hook-language-selector]', () => import('@components/language-selector'))
 moduleInit.async(
   '[js-hook-mobile-floating-button]',
