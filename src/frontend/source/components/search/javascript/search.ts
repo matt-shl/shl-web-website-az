161140315cd574f621a61ec79fb7ddc29e77681b
CU DEPT Anne Vandenhoogen <cu.dept.anne.vandenhoogen@shlgroup.onmicrosoft.com>
2024-08-26 13:48:58 +0000

    Merged PR 1526: SM-512: search flyout

---
+++ src/frontend/source/components/search/javascript/search.ts
@@ -0,0 +1,57 @@
+import Events from "@utilities/events";
+import { KeyboardEvents } from "swiper/types";
+
+const JS_HOOK_FORM = '[js-hook-search-form]'
+const JS_HOOK_INPUT = '[js-hook-search-input]'
+const JS_HOOK_INPUT_RESET = '[js-hook-search-input-reset]'
+
+const HIDDEN_CLASS = 'u-hidden'
+
+class Search {
+  element: HTMLElement
+  private form: HTMLFormElement | null;
+  private input: HTMLInputElement | null;
+  private inputReset: HTMLButtonElement | null;
+
+  constructor(element: HTMLElement) {
+    this.element = element
+    this.form = this.element.querySelector(JS_HOOK_FORM)
+    this.input = this.element.querySelector(JS_HOOK_INPUT)
+    this.inputReset = this.element.querySelector(JS_HOOK_INPUT_RESET)
+
+    this.#bindEvents()
+  }
+
+  #bindEvents() {
+    this.input?.addEventListener('keydown', (event) => this.onTyping(event))
+
+    this.inputReset?.addEventListener('click', (event) => this.resetInput(event))
+
+    Events.$on('modal[navigation-mobile-search]::open', () => this.onOpenFlyout())
+  }
+
+  onTyping(event: KeyboardEvent) {
+    const valueLength = this.input?.value?.length
+    this.inputReset?.classList[valueLength ? 'remove' : 'add'](HIDDEN_CLASS)
+
+    if (event.key === 'Enter') {
+      this.form?.submit()
+    }
+  }
+
+  onOpenFlyout() {
+    this.input?.focus()
+  }
+
+  resetInput(event: Event) {
+    event.preventDefault()
+    if (this.input) {
+      this.input.value = ''
+      this.input.focus()
+    }
+
+    this.inputReset?.classList.add(HIDDEN_CLASS)
+  }
+}
+
+export default Search
