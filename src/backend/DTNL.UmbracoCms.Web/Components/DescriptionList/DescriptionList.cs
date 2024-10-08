e144936482beb51ffade37a343f890bbac41f49b
Kristina Spasevska <kristina.spasevska@deptagency.com>
2024-10-08 16:45:44 +0200

    chore: remove extra method overload

--- src/backend/DTNL.UmbracoCms.Web/Components/DescriptionList/DescriptionList.cs
+++ src/backend/DTNL.UmbracoCms.Web/Components/DescriptionList/DescriptionList.cs
@@ -12,48 +12,10 @@ public partial class DescriptionList
 
     public Button? DownloadLinkButton { get; set; }
 
-
-    public static DescriptionList? Create(PageProduct productPage)
-    {
-        NestedBlockProductSpecifications? productSpecificationsBlock =
-            productPage.Specifications.GetSingleContentOrNull<NestedBlockProductSpecifications>();
-
-        List<DescriptionListItem> items =
-            DescriptionListItem.CreateFor(productPage)
-            .Concat(productSpecificationsBlock is not null ? productSpecificationsBlock.Specifications
-                .Using(s => s.Content as NestedBlockProductSpecification)
-                .Using(DescriptionListItem.Create) : [])
-            .ToList();
-
-        if (items.Count == 0)
-        {
-            return null;
-        }
-
-        return new DescriptionList
-        {
-            Title = productSpecificationsBlock?.Title,
-            Items = items,
-            DownloadLinkButton = Button.Create(Link.Create(productPage?.SpecificationsFile))
-                .With(b =>
-                {
-                    b.Class = "description-list__cta";
-                    b.IconPosition = "start";
-                    b.Icon = SvgAliases.Icons.ArrowTopRight;
-                }),
-        };
-
-    }
-
     public static DescriptionList? Create(
         NestedBlockProductSpecifications? productSpecificationsBlock,
         PageProduct? productPage)
     {
-        if (productSpecificationsBlock is null || productPage is null)
-        {
-            return null;
-        }
-
         List<DescriptionListItem> items = new List<DescriptionListItem>();
         string title = "";
 
@@ -69,7 +31,7 @@ public partial class DescriptionList
         else
         {
             NestedBlockProductSpecifications? productSpecifications =
-            productPage.Specifications.GetSingleContentOrNull<NestedBlockProductSpecifications>();
+            productPage?.Specifications.GetSingleContentOrNull<NestedBlockProductSpecifications>();
 
             items = DescriptionListItem.CreateFor(productPage)
                 .Concat(productSpecifications is not null ? productSpecifications.Specifications
