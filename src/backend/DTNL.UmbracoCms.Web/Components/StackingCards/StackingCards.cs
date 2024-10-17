b7b0cd3eae0e22324f7ae2f4bf1136620c5dad60
Kristina Spasevska <kristina.spasevska@deptagency.com>
2024-10-17 22:43:38 +0200

    feat: add stacking cards

---
+++ src/backend/DTNL.UmbracoCms.Web/Components/StackingCards/StackingCards.cs
@@ -0,0 +1,65 @@
+using Umbraco.Cms.Web.Common.PublishedModels;
+using uSync.Core;
+
+namespace DTNL.UmbracoCms.Web.Components;
+
+
+
+public class StackingCards
+{
+    public required string Title { get; set; }
+
+    public required string Description { get; set; }
+
+    public List<StackingCardItem> Cards { get; set; } = new();
+
+    private static Dictionary<string, string> ThemeMapping = new Dictionary<string, string>()
+    {
+        {"t-dark-green", "t-pastel-green"},
+        {"t-dark-pink", "t-lightest-pink"},
+        {"t-general", "t-lightest-blue"},
+        {"t-light-blue", "t-lightest-blue"},
+        {"t-light-grey", "t-general"},
+        {"t-lightest-blue", "t-general"},
+        {"t-lightest-pink", "t-dark-pink"},
+        {"t-lightest-yellow", "t-pale-yellow"},
+        {"t-pale-blue", "t-general"},
+        {"t-pale-green", "t-dark-green"},
+        {"t-pale-pink", "t-dark-pink"},
+        {"t-pale-yellow", "t-lightest-yellow"},
+        {"t-pastel-blue", "t-general"},
+        {"t-pastel-green", "t-dark-green"},
+        {"t-white", "t-general"},
+        {"t-white-pink", "t-dark-pink"}
+    };
+
+    public required string ThemeOdd { get; set; }
+    public required string ThemeEven { get; set; }
+    public static StackingCards? Create(NestedBlockStackingCards stackingCardsBlock)
+    {
+        List<StackingCardItem> cards = stackingCardsBlock?.Cards?
+            .Select(block => block.Content as NestedBlockStackingCard)
+            .WhereNotNull()
+            .Select(StackingCardItem.Create)
+            .ToList() ?? [];
+
+        if (cards.Count == 0)
+        {
+            return null;
+        }
+
+        string themeOdd = stackingCardsBlock?.FirstCardColor is not null ? $"t-{stackingCardsBlock?.FirstCardColor?.Label}" : "t-lightest-blue";
+        string themeEven = ThemeMapping[themeOdd];
+
+
+        return new StackingCards
+        {
+            Title = stackingCardsBlock?.Title!,
+            Description = stackingCardsBlock?.Description!,
+            Cards = cards,
+            ThemeOdd = themeOdd,
+            ThemeEven = themeEven,
+
+        };
+    }
+}
