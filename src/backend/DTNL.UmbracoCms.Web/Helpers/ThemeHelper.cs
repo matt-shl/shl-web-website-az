531a4d83700df5918a836fc401181964bd702066
Kristina Spasevska <kristina.spasevska@deptagency.com>
2024-11-01 13:14:09 +0100

    chore: remove extra check on themes

--- src/backend/DTNL.UmbracoCms.Web/Helpers/ThemeHelper.cs
+++ src/backend/DTNL.UmbracoCms.Web/Helpers/ThemeHelper.cs
@@ -8,11 +8,6 @@ public static class ThemeHelper
 {
     public static string GetCssClasses(IPublishedContent? page)
     {
-        if (page?.ContentType.Alias is "pageError")
-        {
-            return "light-blue".EnsureStartsWith("t-");
-        }
-
         return GetCssClasses((page as IPageTheme)?.PageTheme, "light-blue");
     }
 
