1ea66a3992902ea6d2d8a78b6134f201d4ed0d0a
CU DEPT Anne Vandenhoogen <cu.dept.anne.vandenhoogen@shlgroup.onmicrosoft.com>
2024-11-25 12:46:09 +0000

    Merged PR 1847: SM-000: form-jsonp
    SM-000: form-jsonp

--- src/frontend/source/components/pardot-form/javascript/pardot-form.ts
+++ src/frontend/source/components/pardot-form/javascript/pardot-form.ts
@@ -1,5 +1,6 @@
 import Form from "@components/form";
 import Events from "@utilities/events";
+import { jsonp } from "@utilities/jsonp";
 
 class PardotForm extends Form {
   element: HTMLFormElement
@@ -11,6 +12,28 @@ class PardotForm extends Form {
 
     this.successText = this.form.dataset.successText || 'Success'
     this.errorText = this.form.dataset.errorText || 'Sorry, something went wrong.'
+  }
+
+  submitForm = (_data: any) => {
+    const inputsMapped = this.inputs.map(fe => ({ key: fe.name, value: fe.value }));
+    const queryParams = inputsMapped.map(({ key, value }) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&');
+    this.action = `${this.action}?${queryParams}`;
+
+    // let jsonp submit the form
+    jsonp(this.action, (data) => {
+      Events.$trigger('loader::hide')
+
+      console.log(data); // Temp, keep in here to be able to test on test env
+
+      // see if 'submitform=true' is in the returned data (URL)
+      if (data.indexOf('submitform=true') > -1) {
+        this.afterSubmitFormSuccess(data)
+      } else {
+        this.afterSubmitFormError(data)
+      }
+
+    })
   }
 
   afterSubmitFormError = (_data: any) => {
@@ -25,14 +48,6 @@ class PardotForm extends Form {
     })
 
     this.resetForm()
-  }
-
-  submitForm = (_data: any) => {
-    const inputsMapped = this.inputs.map(fe => ({ key: fe.name, value: fe.value }));
-    const queryParams = inputsMapped.map(({ key, value }) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
-      .join('&');
-    this.action = `${this.action}&${queryParams}`;
-    super.submitForm(_data)
   }
 }
 
