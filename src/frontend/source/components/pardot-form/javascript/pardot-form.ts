e44b4af8dcc3d32ce8c24edd98bad3c3debbfc8c
CU DEPT Anne Vandenhoogen <cu.dept.anne.vandenhoogen@shlgroup.onmicrosoft.com>
2024-11-26 08:52:51 +0000

    Merged PR 1854: SM-000: form iframe hack
    SM-000: form iframe hack

--- src/frontend/source/components/pardot-form/javascript/pardot-form.ts
+++ src/frontend/source/components/pardot-form/javascript/pardot-form.ts
@@ -1,39 +1,49 @@
 import Form from "@components/form";
 import Events from "@utilities/events";
-import { jsonp } from "@utilities/jsonp";
 
 class PardotForm extends Form {
   element: HTMLFormElement
   private successText: string;
   private errorText: string;
+  private iframe: HTMLIFrameElement;
 
   constructor(element: HTMLFormElement) {
     super(element)
 
+    this.iframe = this.form.querySelector("iframe[name='responseFrame']") as HTMLIFrameElement;
     this.successText = this.form.dataset.successText || 'Success'
     this.errorText = this.form.dataset.errorText || 'Sorry, something went wrong.'
+
+    this.bindEvents()
+  }
+
+  bindEvents() {
+    this.iframe.addEventListener("load",  () => this.loadIframe());
+  }
+
+  loadIframe() {
+    if(!this.iframe.src) return
+    //@ts-ignore
+    const iframeDoc = this.iframe.contentDocument || this.iframe.contentWindow.document;
+    const responseText = iframeDoc.body.textContent;
+
+    if(!responseText) return
+
+    Events.$trigger('loader::hide')
+    // this.iframe.src = '' // Temp turn off to keep response visible for testing
+
+    if (responseText.includes('submitForm=true')) {
+      this.afterSubmitFormSuccess(responseText)
+    } else {
+      this.afterSubmitFormError(responseText)
+    }
   }
 
   submitForm = (_data: any) => {
     const inputsMapped = this.inputs.map(fe => ({ key: fe.name, value: fe.value }));
     const queryParams = inputsMapped.map(({ key, value }) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
       .join('&');
-    this.action = `${this.action}?${queryParams}`;
-
-    // let jsonp submit the form
-    jsonp(this.action, (data) => {
-      Events.$trigger('loader::hide')
-
-      console.log(data); // Temp, keep in here to be able to test on test env
-
-      // see if 'submitform=true' is in the returned data (URL)
-      if (data.indexOf('submitform=true') > -1) {
-        this.afterSubmitFormSuccess(data)
-      } else {
-        this.afterSubmitFormError(data)
-      }
-
-    })
+    this.iframe.src = `${this.action}?${queryParams}`
   }
 
   afterSubmitFormError = (_data: any) => {
