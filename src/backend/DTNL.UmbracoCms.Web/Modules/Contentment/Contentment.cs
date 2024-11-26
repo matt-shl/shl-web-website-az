24b6d69d7c362845764cfcfa9836282ddc2c78bb
Pedro Pontes <pedro.pontes@deptagency.com>
2024-11-26 22:09:06 +0100

    re-enable contentment workaround

--- src/backend/DTNL.UmbracoCms.Web/Modules/Contentment/Contentment.cs
+++ src/backend/DTNL.UmbracoCms.Web/Modules/Contentment/Contentment.cs
@@ -3,13 +3,17 @@ using Umbraco.Cms.Core.Manifest;
 
 namespace DTNL.UmbracoCms.Web.Modules.Contentment;
 
+/// <summary>
+/// Workaround for issue with contentment
+/// See https://our.umbraco.com/packages/backoffice-extensions/contentment/contentment-feedback/112936-angular-error-after-upgrade
+/// </summary>
 public sealed class ContentmentComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
     {
-        //builder
-        //    .ManifestFilters()
-        //    .Append<ContentmentManifestFilter>();
+        builder
+            .ManifestFilters()
+            .Append<ContentmentManifestFilter>();
     }
 }
 
@@ -17,12 +21,8 @@ public sealed class ContentmentManifestFilter : IManifestFilter
 {
     public void Filter(List<PackageManifest> manifests)
     {
-        // Find the existing manifest, or create a new one if it doesn't exist
-        var manifest = manifests.FirstOrDefault(m => m.PackageId == "Umbraco.Community.Contentment");
-
-        if (manifest != null)
+        if (manifests.FirstOrDefault(m => m.PackageId == "Umbraco.Community.Contentment") is { } manifest)
         {
-            // Modify the existing manifest
             manifest.BundleOptions = BundleOptions.Independent;
         }
     }
