13180ac63b5d07cff35059beab730b8d53173744
Anne van den Hoogen <anne.vandenhoogen@deptagency.com>
2024-12-02 10:54:32 +0100

    SM-1265: BE - Hidden field are not saved to Pardot

--- src/frontend/source/components/pardot-form/javascript/pardot-form.ts
+++ src/frontend/source/components/pardot-form/javascript/pardot-form.ts
@@ -2,13 +2,16 @@ import Form from "@components/form";
 
 class PardotForm extends Form {
   element: HTMLFormElement
+  hiddenInputs: HTMLInputElement[]
 
   constructor(element: HTMLFormElement) {
     super(element)
+
+    this.hiddenInputs = [...element.querySelectorAll<HTMLInputElement>('input[type="hidden"]')]
   }
 
   submitForm = (_data: any) => {
-    const inputsMapped = this.inputs.map(fe => ({ key: fe.name, value: fe.value }));
+    const inputsMapped = [...this.inputs, ...this.hiddenInputs].map(fe => ({ key: fe.name, value: fe.value }));
     const queryParams = inputsMapped.map(({ key, value }) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
       .join('&');
 
