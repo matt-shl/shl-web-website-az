ce4e5a3c9076993900a192bc66cf0c4c09ba40c6
Pedro Pontes <pedro.pontes@deptagency.com>
2024-12-03 15:49:51 +0100

    improve exception handeling when trying to retrieve external asset content

--- src/backend/DTNL.UmbracoCms.Web/Services/Assets/ExternalAssetsProvider.cs
+++ src/backend/DTNL.UmbracoCms.Web/Services/Assets/ExternalAssetsProvider.cs
@@ -16,7 +16,7 @@ public static class ExternalAssetsProvider
         {
             return string.Empty;
         }
-        catch (FlurlHttpException)
+        catch (Exception)
         {
             return null;
         }
