26c75ccad7bb6a9d1929473075b5cbbdc0fc765c
Pedro Pontes <pedro.pontes@deptagency.com>
2024-12-16 14:13:43 +0100

    SM-1339

--- src/backend/DTNL.UmbracoCms.Web/Components/CardKnowledge/CardKnowledge.cs
+++ src/backend/DTNL.UmbracoCms.Web/Components/CardKnowledge/CardKnowledge.cs
@@ -7,6 +7,8 @@ public class CardKnowledge : ICard, IOverviewItem
 {
     public required string Title { get; set; }
 
+    public string? Type { get; set; }
+
     public string? Category { get; set; }
 
     public string? Text { get; set; }
@@ -33,6 +35,7 @@ public class CardKnowledge : ICard, IOverviewItem
         return new CardKnowledge
         {
             Title = page.GetTitle(),
+            Type = (page as ICompositionContentDetails)?.Type?.FirstOrDefault(),
             Category = page.GetCategory(),
             Text = page.GetCardDescription(),
             Image = Image.Create(page.GetCardImage(), cssClasses: "card-knowledge__image", style: "card-knowledge"),
@@ -46,6 +49,7 @@ public class CardKnowledge : ICard, IOverviewItem
         return new CardKnowledge
         {
             Title = page.GetTitle(),
+            Type = (page as ICompositionContentDetails)?.Type?.FirstOrDefault(),
             Category = page.GetCategory(),
             Text = page.GetCardDescription(),
             Image = Image.Create(page.GetCardImage(), cssClasses: "card-knowledge__image", style: "card-knowledge"),
